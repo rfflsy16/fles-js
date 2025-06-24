d08d461a0c130e6769a20a6122788e8913458b5c
rfflsy16 <rfflsy16@gmail.com>
2025-06-24 09:01:58 +0700

    feat: delete all

--- src/templates/project/types-template.ts
+++
@@ -1,9 +0,0 @@
-export function createTypesIndexTs(): string {
-    return `import { FlesRequest, FlesResponse, BaseEntity, BaseRepository, BaseService } from "fles-js";
-
-// Controller type for route handlers
-export type Controller = (req: FlesRequest, res: FlesResponse) => Promise<void> | void;
-
-// Add your custom types here
-`;
-}
